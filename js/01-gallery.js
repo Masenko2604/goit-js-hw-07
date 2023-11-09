e900bfdd3b9cc81028ad7ff9c30808a653ffe6cd
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2023-11-09 18:50:09 +0100

    е-07

---
+++ js/01-gallery.js
@@ -0,0 +1,25 @@
+import { galleryItems } from './gallery-items.js';
+
+console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+galleryContainer.addEventListener('click', onImgClick);
+
+// rendered items
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ previw, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+      <img class="gallery__image"
+      src="${preview}" 
+      data-source="${original} 
+      alt="${description}"/>
+      </a>
+    </div>`
+    })
+    .join('');
+  
+}
